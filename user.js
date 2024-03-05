75b85fe26cc42588ac19e1ecc55c1efe17c34d6a
Neha Ambasta <nehaambasta08@gmail.com>
2024-03-04 19:41:01 -0500

    updated cors for user

--- user.js
+++ user.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const mongodb = require('mongoose')
 const app = express()
+var cors = require('cors')
 const multer = require('multer');
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // Number of salt rounds for bcrypt
-
+app.use(cors);
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json());
